cbbfbea83c1ea38276fa1b28417d65b6b74f50a7
karlitoxz <electronicaysistemas@gmail.com>
2021-09-30 15:49:05 -0500

    Cambiar la forma en que llamo a la libreria.

--- CalculadoraTest.php
+++ CalculadoraTest.php
@@ -1,15 +1,16 @@
 <?php 
 
 require '../../phpunit/vendor/autoload.php';
+use \PHPUnit\Framework\TestCase;
 
-final class CalculadoraTest extends \PHPUnit\Framework\TestCase 
+final class CalculadoraTest extends TestCase
 {
 
 	public function testSuma()
 	{
 		$calc = new Calculadora();
 			$this->assertEquals(
-				8, $calc->suma(3,4)
+				7, $calc->suma(3,4)
 			);
 	}
 
